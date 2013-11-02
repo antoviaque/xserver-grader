0bf49d9b1c9cc9aa98e93120acfd9783b3b33e2d
Xavier Antoviaque <xavier@antoviaque.org>
2013-11-02 09:36:07 +0100

    Mock grade.grade() returned result

--- grade.py
+++ grade.py
@@ -1,2 +1,10 @@
 def grade(grader_path, grader_config, student_response, sandbox):
     print grader_path, grader_config, student_response, sandbox
+    
+    results = {
+        'correct': True,
+        'score': 1,
+        'tests': [],
+        'errors': []
+    }
+    return results
